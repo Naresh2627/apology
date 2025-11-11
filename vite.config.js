37803ab9b71679a14b0b70d3abce1062a5c8e4ab
nareshsknds <naresh.s@senatio.com>
2025-11-11 14:45:45 +0530

    Fix Vercel deployment - simplified configuration

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild'
   }
 })
